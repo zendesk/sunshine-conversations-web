ce0a3414e73aa1b53267dc9fe1cb523dc1244586
Andrew Lavers <andrew.lavers@radialpoint.com>
2015-01-30 12:27:36 -0500

    Unread badge

--- src/js/headerView.js
+++ src/js/headerView.js
@@ -6,12 +6,18 @@ var headerView = Backbone.View.extend({
     events: {
         "click #sk-handle": "toggle"
     },
+
+    initialize: function() {
+        this.listenTo(this.model, 'change:unread', this.render);
+    },
+
     render: function() {
         this.$el.html(template({
-            count: 2000
+            count: this.model.unread
         }));
         return this;
     },
+
     toggle: function() {
         if (window.SupportKit && window.SupportKit.ui && window.SupportKit.ui.toggle) {
             window.SupportKit.ui.toggle();
