63bf0f33576ea362a085c276e8ce897ef670eccc
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-05-28 17:14:40 -0400

    added junit reporter

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,13 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'junit'],
+
+        // the default configuration
+        junitReporter: {
+            outputFile: 'test-results.xml',
+            suite: ''
+        },
 
 
         // web server port
