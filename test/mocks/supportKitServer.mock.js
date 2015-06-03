a99471a9c0c3e4e22dc062841ff1559ea4319632
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-06-03 14:37:20 -0400

    simplified skserver mock

--- test/mocks/supportKitServer.mock.js
+++ test/mocks/supportKitServer.mock.js
@@ -7,47 +7,40 @@ var userStore = require('../data/users'),
     conversationStore = require('../data/conversations');
 
 module.exports = BaseServerMock.extend({
-    routes: function() {
-        var faye = require('faye');
-        var endpoint = require('../../src/js/endpoint');
-
-        var routes = [
-            [
-                'OPTIONS', /.*/, [200, {
-                    'Access-Control-Allow-Origin': '*',
-                    'Vary': 'Origin',
-                    'Access-Control-Allow-Methods': ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-                    'Access-Control-Allow-Headers': ['accept', 'app-token', 'content-type'],
-                    'Connection': 'keep-alive'
-                }, '']
-            ],
-            [
-                'GET', /\/faye.*/, [200, {}, '']
-            ],
-            [
-                'POST', /\/api\/appboot/, [200, {
-                    'Content-Type': 'application/json'
-                }, JSON.stringify({
-                    appUserId: 1
-                })]
-            ],
-            [
-                'PUT', /\/api\/appusers\/([a-z0-9]+)/, function(xhr, id) {
-                    xhr.respond(200, {}, JSON.stringify(userStore[id]))
-                }
-            ],
-            [
-                'GET', /\/api\/conversations/, [200, {
-                    'Content-Type': 'application/json'
-                }, JSON.stringify(_(conversationStore).values())]
-            ],
-            [
-                'POST', /\/api\/conversations\/([a-z0-9]+)\/messages/, function(xhr, id) {
-                    xhr.respond(200, {}, JSON.stringify(xhr.requestBody))
-                }
-            ],
-        ];
-
-        return routes;
-    }
+    routes: [
+        [
+            'OPTIONS', /.*/, [200, {
+                'Access-Control-Allow-Origin': '*',
+                'Vary': 'Origin',
+                'Access-Control-Allow-Methods': ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+                'Access-Control-Allow-Headers': ['accept', 'app-token', 'content-type'],
+                'Connection': 'keep-alive'
+            }, '']
+        ],
+        [
+            'GET', /\/faye.*/, [200, {}, '']
+        ],
+        [
+            'POST', /\/api\/appboot/, [200, {
+                'Content-Type': 'application/json'
+            }, JSON.stringify({
+                appUserId: 1
+            })]
+        ],
+        [
+            'PUT', /\/api\/appusers\/([a-z0-9]+)/, function(xhr, id) {
+                xhr.respond(200, {}, JSON.stringify(userStore[id]))
+            }
+        ],
+        [
+            'GET', /\/api\/conversations/, [200, {
+                'Content-Type': 'application/json'
+            }, JSON.stringify(_(conversationStore).values())]
+        ],
+        [
+            'POST', /\/api\/conversations\/([a-z0-9]+)\/messages/, function(xhr, id) {
+                xhr.respond(200, {}, JSON.stringify(xhr.requestBody))
+            }
+        ],
+    ]
 });
