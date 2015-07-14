02700fa6345b75d0192968f268fc3c994e163aa2
SupportKit <hello@supportkit.io>
2015-07-14 19:32:17 +0000

    Release v0.2.14 [ci skip]

--- src/js/main.js
+++ src/js/main.js
@@ -34,7 +34,7 @@ require('../stylesheets/main.less');
  * Contains all SupportKit API classes and functions.
  */
 var SupportKit = Marionette.Object.extend({
-    VERSION: '0.2.13',
+    VERSION: '0.2.14',
 
     defaultText: {
         headerText: 'How can we help?',
