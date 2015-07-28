b0fadc8a0fdf98d285f72e8eaa8a5f7faeab7a89
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-07-28 16:20:34 -0400

    brought back isDirty and added tests for appUser

--- src/js/models/appUser.js
+++ src/js/models/appUser.js
@@ -6,6 +6,10 @@ var _ = require('underscore'),
     BaseModel = require('./baseModel');
 
 var AppUser = module.exports = BaseModel.extend({
+    initialize: function() {
+        this._lastPropertyValues = this.pick(AppUser.EDITABLE_PROPERTIES);
+    },
+
     parse: function(data) {
         return _.isObject(data) ? data : {
             id: data
@@ -21,10 +25,27 @@ var AppUser = module.exports = BaseModel.extend({
             properties: {},
             conversationStarted: false
         };
+    },
+
+    isDirty: function() {
+        return _.some(AppUser.EDITABLE_PROPERTIES, function(property) {
+            return this._lastPropertyValues[property] !== this.get(property);
+        }.bind(this));
+    },
+
+    save: function() {
+        if (this.isDirty()) {
+            this._lastPropertyValues = this.pick(AppUser.EDITABLE_PROPERTIES);
+            return Backbone.Model.prototype.save.apply(this, _.toArray(arguments));
+        } else {
+            return $.Deferred().resolve(this, null, null);
+        }
     }
 }, {
-    EDITABLE_PROPERTIES: ['givenName',
+    EDITABLE_PROPERTIES: [
+        'givenName',
         'surname',
         'email',
-        'properties']
+        'properties'
+    ]
 });
---
+++ test/specs/appUser.spec.js
@@ -0,0 +1,105 @@
+var sinon = require('sinon'),
+    AppUser = require('../../src/js/models/appUser');
+
+var ClientScenario = require('../scenarios/clientScenario');
+
+describe('ChatController', function() {
+    var scenario,
+        sandbox,
+        user;
+
+    before(function() {
+        scenario = new ClientScenario();
+        scenario.build();
+
+        sandbox = sinon.sandbox.create();
+    });
+
+    after(function() {
+        scenario.clean();
+    });
+
+    describe('#parse', function() {
+        describe('with object', function() {
+            it('should set the object in the attributes', function() {
+                var email = 'some@email.com',
+                    givenName = 'Some',
+                    surname = 'Name';
+
+                var user = new AppUser({
+                    email: email,
+                    givenName: givenName,
+                    surname: surname
+                }, {
+                    parse: true
+                });
+
+                user.get('email').should.equal(email);
+                user.get('givenName').should.equal(givenName);
+                user.get('surname').should.equal(surname);
+
+            });
+        });
+
+        describe('with id', function(){
+            it('should map the id in the attributes', function(){
+                var id = '12345';
+
+                var user = new AppUser(id, {
+                    parse: true
+                });
+
+                user.id.should.equals(id);
+                user.get('id').should.equals(id);
+            });
+        });
+    });
+
+    describe('#isDirty', function() {
+            it('should mark the user as dirty after a set', function() {
+                var email = 'some@email.com',
+                    givenName = 'Some',
+                    surname = 'Name';
+
+                var user = new AppUser({
+                    email: email,
+                    givenName: givenName,
+                    surname: surname
+                }, {
+                    parse: true
+                });
+
+                user.isDirty().should.be.false;
+
+                user.set('email', 'other@email.com');
+
+                user.isDirty().should.be.true;
+            });
+
+            it('should not be marked as dirty if changes are reverted', function() {
+                var email = 'some@email.com',
+                    givenName = 'Some',
+                    surname = 'Name';
+
+                var user = new AppUser({
+                    email: email,
+                    givenName: givenName,
+                    surname: surname
+                }, {
+                    parse: true
+                });
+
+                user.isDirty().should.be.false;
+
+                user.set('email', 'other@email.com');
+
+                user.isDirty().should.be.true;
+
+                user.set('email', email);
+
+                user.isDirty().should.be.false;
+            });
+
+    });
+
+});
