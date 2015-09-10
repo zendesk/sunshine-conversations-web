96197bfac8944640a2b9afd314553be36dce2f76
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-09-10 13:56:27 -0400

    release notes for 0.2.29

--- src/js/main.js
+++ src/js/main.js
@@ -34,7 +34,7 @@ require('../stylesheets/main.less');
  * Contains all SupportKit API classes and functions.
  */
 var SupportKit = Marionette.Object.extend({
-    VERSION: '0.2.27',
+    VERSION: '0.2.28',
 
     defaultText: {
         headerText: 'How can we help?',
