c9e4a3046d40ed0c0651ffdd780917b54c63d44b
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-09-10 14:40:13 -0400

    fix maxcdn taskname and release note for 0.2.30

--- src/js/main.js
+++ src/js/main.js
@@ -34,7 +34,7 @@ require('../stylesheets/main.less');
  * Contains all SupportKit API classes and functions.
  */
 var SupportKit = Marionette.Object.extend({
-    VERSION: '0.2.28',
+    VERSION: '0.2.29',
 
     defaultText: {
         headerText: 'How can we help?',
