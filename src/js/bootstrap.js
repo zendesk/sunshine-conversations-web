24b43aedd42bc96b5f1cdf831538db54a2c18cfc
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-09-16 10:52:30 -0400

    readded getPrototypeOf polyfill

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -8,6 +8,21 @@ var $ = require('jquery');
 
 require('./utils/jquery.support.cssproperty');
 
+// Polyfill Object.getPrototypeOf
+// http://ejohn.org/blog/objectgetprototypeof/
+if (typeof Object.getPrototypeOf !== 'function') {
+    if (typeof 'test'.__proto__ === 'object') {
+        Object.getPrototypeOf = function(object) {
+            return object.__proto__;
+        };
+    } else {
+        Object.getPrototypeOf = function(object) {
+            // May break if the constructor has been tampered with
+            return object.constructor.prototype;
+        };
+    }
+}
+
 /**
  * Marionette setup
  */
