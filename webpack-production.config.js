283c11aa17f4639e93f9254603cf6a2df07cf551
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-12-01 09:47:04 -0500

    fixed sinon version, updated production build config, changed package.json entry point

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,11 +1,4 @@
-module.exports = [
-    require('./make-webpack-config')({
-        longTermCaching: true,
-        minimize: true,
-        devtool: "source-map"
-    }),
-    require('./make-webpack-config')({
-        prerender: true,
-        minimize: true
-    })
-];
+module.exports = require('./make-webpack-config')({
+    minimize: true,
+    devtool: 'source-map'
+});
