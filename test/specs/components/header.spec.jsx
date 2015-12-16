a39cf2dc96dbf3733650a0e6ffecf862c21f53ea
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-12-16 00:53:19 -0500

    added click simulation test on header

--- test/specs/components/header.spec.jsx
+++ test/specs/components/header.spec.jsx
@@ -7,38 +7,59 @@ import { HeaderComponent } from 'components/header.jsx';
 
 const sandbox = sinon.sandbox.create();
 
+const defaultProps = {
+    appState: {
+        widgetOpened: false,
+        settingsEnabled: false,
+        settingsVisible: false,
+    },
+    actions: {
+        showSettings: sandbox.spy(),
+        hideSettings: sandbox.spy(),
+        toggleWidget: sandbox.spy()
+    },
+    ui: {
+        text: {
+            headerText: 'Header',
+            settingsHeaderText: 'Settings'
+        }
+    }
+};
+
 describe('Header', () => {
     afterEach(() => {
         sandbox.restore();
     });
 
     describe('main view', () => {
-        const props = {
-            appState: {
-                widgetOpened: false,
-                settingsEnabled: false,
-                settingsVisible: false,
-            },
-            actions: {
-                showSettings: sandbox.spy(),
-                hideSettings: sandbox.spy()
-            },
-            ui: {
-                text: {
-                    headerText: 'Header',
-                    settingsHeaderText: 'Settings'
-                }
-            }
-        };
+        const props = Object.assign({}, defaultProps);
+
+        var header = TestUtils.renderIntoDocument(<HeaderComponent {...props} />);
+        var headerNode = ReactDOM.findDOMNode(header);
 
         it('should display the main header', () => {
-            var header = TestUtils.renderIntoDocument(
-            <HeaderComponent {...props} />
-            );
-
-            var headerNode = ReactDOM.findDOMNode(header);
-
             headerNode.textContent.should.eq(props.ui.text.headerText);
         });
-    })
+
+        it('should call the toggleWidget action on header click', () => {
+            TestUtils.Simulate.click(headerNode);
+            props.actions.toggleWidget.should.have.been.calledOnce;
+        })
+    });
+
+
+    describe('settings view', () => {
+        const props = Object.assign({}, defaultProps, {
+            appState: {
+                settingsEnabled: true,
+                settingsVisible: true
+            }
+        });
+        var header = TestUtils.renderIntoDocument(<HeaderComponent {...props} />);
+        var headerNode = ReactDOM.findDOMNode(header);
+
+        it('should display the settings header', () => {
+            headerNode.textContent.should.eq(props.ui.text.settingsHeaderText);
+        });
+    });
 });
