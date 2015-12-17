f7941836847c475a90e713ce81bca3fa370fb85e
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-12-17 08:23:48 -0500

    finished test suite for message component

--- test/specs/components/message.spec.js
+++ test/specs/components/message.spec.js
@@ -41,30 +41,115 @@ describe('ChatInput', () => {
             row.className.indexOf('sk-left-row').should.be.eq(-1);
         });
 
-    });
-
-
-    describe('appMaker', () => {
-        const props = Object.assign({}, defaultProps, {
-            role: 'appMaker',
-            avatarUrl: 'http://some-image.url'
+        it('should not put an author name on the message', () => {
+            const fromNode = TestUtils.findRenderedDOMComponentWithClass(component, 'sk-from');
+            fromNode.textContent.should.be.eq('');
         });
 
-        beforeEach(() => {
-            component = TestUtils.renderIntoDocument(<Message {...props} />);
-            componentNode = ReactDOM.findDOMNode(component);
+        it('should not contain any actions', () => {
+            TestUtils.scryRenderedDOMComponentsWithClass(component, 'sk-action').length.should.be.eq(0);
         });
 
-        it('should have an avatar', () => {
-            TestUtils.scryRenderedDOMComponentsWithClass(component, 'sk-msg-avatar').length.should.be.eq(1);
-        });
-
-
-        it('should be on the left', () => {
-            const row = TestUtils.findRenderedDOMComponentWithClass(component, 'sk-row');
-            row.className.indexOf('sk-left-row').should.be.gte(0);
-            row.className.indexOf('sk-right-row').should.be.eq(-1);
+        it('should contains the given text', () => {
+            const message = TestUtils.findRenderedDOMComponentWithClass(component, 'sk-msg');
+            message.textContent.should.eq(props.text);
         });
     });
+
+    for (role of ['appMaker', 'whisper']) {
+        describe(`${role} without actions`, () => {
+            const props = Object.assign({}, defaultProps, {
+                role: role,
+                name: 'Smooch',
+                avatarUrl: 'http://some-image.url'
+            });
+
+            beforeEach(() => {
+                component = TestUtils.renderIntoDocument(<Message {...props} />);
+                componentNode = ReactDOM.findDOMNode(component);
+            });
+
+            it('should have an avatar', () => {
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'sk-msg-avatar').length.should.be.eq(1);
+            });
+
+            it('should be on the left', () => {
+                const row = TestUtils.findRenderedDOMComponentWithClass(component, 'sk-row');
+                row.className.indexOf('sk-left-row').should.be.gte(0);
+                row.className.indexOf('sk-right-row').should.be.eq(-1);
+            });
+
+            it('should put an author name on the message', () => {
+                const fromNode = TestUtils.findRenderedDOMComponentWithClass(component, 'sk-from');
+                fromNode.textContent.should.be.eq(props.name);
+            });
+
+            it('should not contain any actions', () => {
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'sk-action').length.should.be.eq(0);
+            });
+
+            it('should contains the given text', () => {
+                const message = TestUtils.findRenderedDOMComponentWithClass(component, 'sk-msg');
+                message.textContent.should.eq(props.text);
+            });
+        });
+
+        describe(`${role} with actions`, () => {
+            const props = Object.assign({}, defaultProps, {
+                role: role,
+                name: `Smooch ${role}`,
+                avatarUrl: 'http://some-image.url',
+                actions: [
+                    {
+                        _id: '1',
+                        text: 'Action 1',
+                        uri: 'http://some-uri/'
+                    },
+                    {
+                        _id: '2',
+                        text: 'Action 2',
+                        uri: 'wss://some-other-uri/'
+                    }
+                ]
+            });
+
+            beforeEach(() => {
+                component = TestUtils.renderIntoDocument(<Message {...props} />);
+                componentNode = ReactDOM.findDOMNode(component);
+            });
+
+            it('should have an avatar', () => {
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'sk-msg-avatar').length.should.be.eq(1);
+            });
+
+            it('should be on the left', () => {
+                const row = TestUtils.findRenderedDOMComponentWithClass(component, 'sk-row');
+                row.className.indexOf('sk-left-row').should.be.gte(0);
+                row.className.indexOf('sk-right-row').should.be.eq(-1);
+            });
+
+            it('should put an author name on the message', () => {
+                const fromNode = TestUtils.findRenderedDOMComponentWithClass(component, 'sk-from');
+                fromNode.textContent.should.be.eq(props.name);
+            });
+
+            it('should contain any actions', () => {
+                const actionNodes = TestUtils.scryRenderedDOMComponentsWithClass(component, 'sk-action');
+                actionNodes.length.should.be.eq(props.actions.length);
+
+                actionNodes.forEach((node, index) => {
+                    const link = node.getElementsByTagName('a')[0];
+                    link.href.should.eq(props.actions[index].uri);
+                    link.textContent.should.eq(props.actions[index].text);
+                });
+            });
+
+            it('should contains the given text', () => {
+                const message = TestUtils.findRenderedDOMComponentWithClass(component, 'sk-msg');
+                // actions text will be added at the end.
+                message.textContent.indexOf(props.text).should.eq(0);
+            });
+        });
+    }
 
 });
