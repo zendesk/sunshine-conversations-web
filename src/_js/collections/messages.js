42f3142362b1bc02e26384cab4e533245dbc39e0
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2016-01-03 21:53:39 -0500

    remove legacy js code

--- src/_js/collections/messages.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var Backbone = require('backbone');
-var Message = require('../models/message');
-var _ = require('underscore');
-
-module.exports = Backbone.Collection.extend({
-    model: Message,
-    comparator: 'received'
-});
