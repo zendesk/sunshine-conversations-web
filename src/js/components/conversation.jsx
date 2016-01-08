bfbfb3fcc07fdfd521039d3af4c5e755822cd7e3
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2016-01-08 15:06:40 -0500

    fixed logo anchoring

--- src/js/components/conversation.jsx
+++ src/js/components/conversation.jsx
@@ -7,6 +7,12 @@ import { createMarkup } from 'utils/html';
 import { MessageComponent } from 'components/message.jsx';
 
 export class ConversationComponent extends Component {
+    constructor(...args) {
+        super(...args);
+        this.state = {
+            logoIsAnchored: true
+        };
+    }
     _scrollToBottom() {
         let container = findDOMNode(this);
         let logo = this.refs.logo;
@@ -14,12 +20,36 @@ export class ConversationComponent extends Component {
         container.scrollTop = scrollTop;
     }
 
+    _positionLogo() {
+        let container = findDOMNode(this);
+        let logo = this.refs.logo;
+        let intro = this.refs.intro;
+        let messages = this.refs.messages;
+        let conversationHeight = container.clientHeight;
+        let logoHeight = logo.clientHeight;
+        let introHeight = intro.clientHeight;
+        let messagesHeight = messages.clientHeight;
+        let heightRemaining = conversationHeight - (introHeight + messagesHeight + logoHeight);
+
+        if (heightRemaining > logoHeight) {
+            this.setState({
+                logoIsAnchored: true
+            });
+        } else {
+            this.setState({
+                logoIsAnchored: false
+            });
+        }
+    }
+
     componentDidMount() {
         setTimeout(this._scrollToBottom.bind(this));
+        setTimeout(this._positionLogo.bind(this));
     }
 
     componentDidUpdate() {
         setTimeout(this._scrollToBottom.bind(this));
+        setTimeout(this._positionLogo.bind(this));
     }
 
     render() {
@@ -28,8 +58,10 @@ export class ConversationComponent extends Component {
         return (
             <div id="sk-conversation" ref="container">
                 <div ref="intro" className="sk-intro" dangerouslySetInnerHTML={ createMarkup(this.props.ui.text.introText) }></div>
-                { messages }
-                <div className="sk-logo" ref="logo">
+                <div ref="messages">
+                    { messages }
+                </div>
+                <div className={this.state.logoIsAnchored ? "sk-logo anchor-bottom" : "sk-logo"} ref="logo">
                     <a href="https://smooch.io/?utm_source=widget" target="_blank"><span>In-App Messaging by</span> <img className="sk-image" src={ require('images/logo_webwidget.png') } alt="Smooch" /> <img className="sk-image-retina" src={ require('images/logo_webwidget_2x.png') } alt="Smooch" /></a>
                 </div>
             </div>
