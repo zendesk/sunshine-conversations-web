da81969d03b4742cc8574980c6df52b53badef95
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2016-01-14 19:53:40 -0500

    handling javascript action uri

--- src/js/components/action.jsx
+++ src/js/components/action.jsx
@@ -101,9 +101,11 @@ export class ActionComponent extends Component {
                     );
             }
         } else {
+            let isJavascript = this.props.ui.startsWith('javascript:');
+
             return (
                 <div className='sk-action'>
-                    <a className='btn btn-sk-primary' href={ this.props.uri } target='_blank'>
+                    <a className='btn btn-sk-primary' href={ this.props.uri } target={ isJavascript ? '_self' : '_blank' }>
                         { this.props.text }
                     </a>
                 </div>
