8915bdfdd059673fab0c15d16a65a359abaa060e
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2016-04-06 15:43:51 -0400

    subscribe to new channel

--- src/js/utils/faye.js
+++ src/js/utils/faye.js
@@ -37,7 +37,7 @@ export function initFaye() {
             }
         });
 
-        return faye.subscribe('/conversations/' + state.conversation._id, (message) => {
+        return faye.subscribe('/v1/conversations/' + state.conversation._id, (message) => {
             store.dispatch(addMessage(message));
             store.dispatch(incrementUnreadCount());
         });
