2c7e7784ec2b45e64fe927aa4d877dc4a27da25b
Mario54 <mariolamontagne54@gmail.com>
2016-05-16 15:16:01 -0400

    Use accent color for loading image animation

--- src/js/components/image-loading.jsx
+++ src/js/components/image-loading.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-export function ImageLoading() {
+export function ImageLoading({accentColor} = {}) {
+    const bounceStyle = {};
+
+    if (accentColor) {
+        bounceStyle.backgroundColor = `#${accentColor}`;
+    }
+
     return <div className='image-overlay'>
                <div className='three-bounce spinner'>
-                   <div className='bounce1'></div>
-                   <div className='bounce2'></div>
-                   <div className='bounce3'></div>
+                   <div className='bounce1'
+                        style={ bounceStyle }></div>
+                   <div className='bounce2'
+                        style={ bounceStyle }></div>
+                   <div className='bounce3'
+                        style={ bounceStyle }></div>
                </div>
            </div>;
 }
