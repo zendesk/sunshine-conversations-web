6a1dc1bcbda23f57f5a232e30c6c4f095541e377
Chloe Pou-Prom <chloe@smooch.io>
2016-05-19 14:55:24 -0400

    Add more tests for conversation-reducer

--- test/specs/reducers/conversation-reducer.spec.js
+++ test/specs/reducers/conversation-reducer.spec.js
@@ -1,10 +1,7 @@
 import { ConversationReducer } from 'reducers/conversation-reducer';
 import { ADD_MESSAGE, REPLACE_MESSAGE, REMOVE_MESSAGE, RESET_CONVERSATION, SET_CONVERSATION, RESET_UNREAD_COUNT, INCREMENT_UNREAD_COUNT } from 'actions/conversation-actions';
 
-const INITIAL_STATE = {
-    messages: [],
-    unreadCount: 0
-};
+const INITIAL_STATE = ConversationReducer(undefined, {});
 const MESSAGE_1 = {
     text: 'hi!',
     role: 'appUser',
@@ -22,12 +19,126 @@ const MESSAGE_2 = {
     _id: '789101'
 };
 const MESSAGES = [MESSAGE_1, MESSAGE_2];
+const UPLOADING_IMAGE = {
+    mediaUrl: 'data:image/jpeg',
+    mediaType: 'image/jpeg',
+    role: 'appUser',
+    status: 'sending',
+    _tempId: 0.8288994217337065,
+    sent: '2016-05-19T18:33:10.788Z'
+};
+const RECEIVED_IMAGE = {
+    text: 'some_media_url',
+    mediaType: 'image/jpeg',
+    mediaUrl: 'some_media_url',
+    role: 'appUser',
+    received: 1463682757.454,
+    authorId: '8a9445dadad4862c2322db52',
+    name: 'Calm Chimpanzee',
+    _id: '573e06c550a52d2900f907c6'
+};
 
 describe('Conversation reducer', () => {
-    it('should have no messages initially', () => {
-        Object.keys(ConversationReducer(undefined, {})).length.should.eq(2);
-        ConversationReducer(undefined, {}).messages.length.should.eq(0);
-        ConversationReducer(undefined, {}).unreadCount.should.eq(0);
+
+    describe('SET_CONVERSATION action', () => {
+        it('should merge state and stores messages', () => {
+            const beforeState = {
+                unreadCount: 5,
+                messages: [MESSAGE_1]
+            };
+            const afterState = ConversationReducer(beforeState, {
+                type: SET_CONVERSATION,
+                conversation: {
+                    messages: [MESSAGE_2],
+                    appUsers: [],
+                    appMakers: []
+                }
+            });
+            afterState.messages.length.should.eq(2);
+            afterState.messages.should.contain(MESSAGE_1);
+            afterState.messages.should.contain(MESSAGE_2);
+        });
+
+        it('should not add a duplicate message', () => {
+            const beforeState = {
+                unreadCount: 4,
+                messages: MESSAGES
+            };
+            const afterState = ConversationReducer(beforeState, {
+                type: SET_CONVERSATION,
+                conversation: {
+                    messages: [MESSAGE_1],
+                    appUsers: [],
+                    appMakers: []
+                }
+            });
+            afterState.messages.length.should.eq(2);
+            afterState.messages.should.eql(MESSAGES);
+        });
+    });
+
+    describe('ADD_MESSAGE action', () => {
+        it('should add message', () => {
+            const beforeState = INITIAL_STATE;
+            const afterState = {
+                messages: [MESSAGE_1],
+                unreadCount: 0
+            };
+            ConversationReducer(beforeState, {
+                type: ADD_MESSAGE,
+                message: MESSAGE_1
+            }).should.eql(afterState);
+        });
+
+        it('should keep uploading image at the end of the messages array', () => {
+            const beforeState = {
+                messages: [MESSAGE_1, UPLOADING_IMAGE],
+                unreadCount: 0
+            };
+            const afterState = ConversationReducer(beforeState, {
+                type: ADD_MESSAGE,
+                message: MESSAGE_2
+            });
+            afterState.messages.length.should.eq(3);
+            afterState.messages[0].should.eql(MESSAGE_1);
+            afterState.messages[1].should.eql(MESSAGE_2);
+            afterState.messages[2].should.eql(UPLOADING_IMAGE);
+        });
+    });
+
+    describe('REPLACE_MESSAGE action', () => {
+        it('should replace uploading image with received image', () => {
+            const beforeState = {
+                messages: [MESSAGE_1, UPLOADING_IMAGE],
+                unreadCount: 0
+            };
+            const afterState = ConversationReducer(beforeState, {
+                type: REPLACE_MESSAGE,
+                message: RECEIVED_IMAGE,
+                queryProps: {
+                    _tempId: UPLOADING_IMAGE._tempId
+                }
+            });
+            afterState.messages.length.should.eq(2);
+            afterState.messages[1].should.not.eql(UPLOADING_IMAGE);
+            afterState.messages[1].should.eql(RECEIVED_IMAGE);
+        });
+
+        it('should not remove anything if message to be removed does not exist', () => {
+            const beforeState = {
+                messages: [MESSAGE_1, UPLOADING_IMAGE],
+                unreadCount: 0
+            };
+            const afterState = ConversationReducer(beforeState, {
+                type: REPLACE_MESSAGE,
+                message: RECEIVED_IMAGE,
+                queryProps: {
+                    _tempId: 1234
+                }
+            });
+            afterState.messages.length.should.eq(2);
+            afterState.should.eql(beforeState);
+        });
     });
 
     it('should set to initial state on RESET_CONVERSATION', () => {
@@ -38,18 +149,6 @@ describe('Conversation reducer', () => {
         const afterState = INITIAL_STATE;
         ConversationReducer(beforeState, {
             type: RESET_CONVERSATION
-        }).should.eql(afterState);
-    });
-
-    it('should add message on ADD_MESSAGES', () => {
-        const beforeState = INITIAL_STATE;
-        const afterState = {
-            messages: [MESSAGE_1],
-            unreadCount: 0
-        };
-        ConversationReducer(beforeState, {
-            type: ADD_MESSAGE,
-            message: MESSAGE_1
         }).should.eql(afterState);
     });
 
