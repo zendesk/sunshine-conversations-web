8e05da31a107dc40bf507d22284c1f2341afa702
Chloe Pou-Prom <chloe@smooch.io>
2016-05-20 15:46:01 -0400

    Create new util for retrieving device id

---
+++ src/js/utils/device.js
@@ -0,0 +1,11 @@
+import { storage } from 'storage';
+
+export function getDeviceId() {
+    const SK_STORAGE = 'sk_deviceid';
+    const deviceId = storage.getItem(SK_STORAGE) ||
+    uuid.v4().replace(/-/g, '');
+
+    storage.setItem(SK_STORAGE, deviceId);
+
+    return deviceId;
+}
