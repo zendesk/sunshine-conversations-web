1ce43bff5ef980ee2db4eea8697b88cc7150bf75
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2016-05-24 17:59:41 -0400

    Revert "Release 2.6.1"

--- test/utils/redux.js
+++ test/utils/redux.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import configureStore from 'redux-mock-store';
 
 const AppStore = require('stores/app-store');
 const store = AppStore.store;
@@ -12,10 +13,15 @@ function restoreAppStore() {
 }
 
 export function createMockedStore(sinon, mockedState = {}) {
-    const store = createStore(() => mockedState);
+    const middlewares = [thunkMiddleware];
+    const mockStore = configureStore(middlewares);
+    const store = mockStore(mockedState);
 
     sinon.spy(store, 'dispatch');
     store.restore = restoreAppStore;
+    store.subscribe = sinon.spy(() => {
+        return function() {};
+    });
     return store;
 }
 
