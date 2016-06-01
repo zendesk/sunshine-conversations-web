cc68082401c1d7241f8f1502e327d8ec1e192503
chloepouprom <chloe@smooch.io>
2016-06-01 15:17:23 -0400

    Fix width of message bubble (#269)
    * Fix width of message bubble
    
    * Fix formatting
    
    * Move getElementProperties function to utils
    
    * Cache result of getComputedStyle

--- src/js/components/message.jsx
+++ src/js/components/message.jsx
@@ -3,12 +3,32 @@ import React, { Component } from 'react';
 import { TextMessage } from './text-message';
 import { ImageMessage } from './image-message';
 import { ActionComponent } from './action';
-
+import { findDOMNode } from 'react-dom';
+import { getElementProperties } from '../utils/dom';
 
 export class MessageComponent extends Component {
     static defaultProps = {
         actions: []
     };
+
+    componentDidMount() {
+        if (this.props.actions.length === 0) {
+            this._restyleBubble();
+        }
+    }
+
+    _restyleBubble() {
+        const bubble = findDOMNode(this.refs.messageContent);
+        if (bubble) {
+            const messageElement = bubble.firstChild;
+            const messageProperties = getElementProperties(messageElement);
+            const bubbleProperties = getElementProperties(bubble);
+            const multiLineCheck = parseInt(bubbleProperties.fontSize) * 2;
+            if (messageProperties.height > multiLineCheck && messageProperties.width < bubbleProperties.width) {
+                bubble.style.width = (messageProperties.width + parseInt(bubbleProperties.paddingLeft) + parseInt(bubbleProperties.paddingRight)) + 'px';
+            }
+        }
+    }
 
     render() {
         const actions = this.props.actions.map((action) => {
@@ -24,7 +44,7 @@ export class MessageComponent extends Component {
             <img className={ avatarClass.join(' ') }
                  src={ this.props.avatarUrl } />
             );
-        const avatarPlaceHolder = isAppUser ? null : (<div className='sk-msg-avatar-placeholder'/>);
+        const avatarPlaceHolder = isAppUser ? null : (<div className='sk-msg-avatar-placeholder' />);
 
         const message = this.props.mediaUrl ?
             <ImageMessage {...this.props} /> :
@@ -39,42 +59,43 @@ export class MessageComponent extends Component {
         const style = {};
 
         if (!this.props.mediaUrl) {
-          if (isAppUser && this.props.accentColor) {
-            style.backgroundColor = style.borderLeftColor = `#${this.props.accentColor}`;
-          }
+            if (isAppUser && this.props.accentColor) {
+                style.backgroundColor = style.borderLeftColor = `#${this.props.accentColor}`;
+            }
         }
         if (this.props.firstInGroup && !this.props.lastInGroup) {
-          if (isAppUser) {
-            containerClass.push('sk-msg-appuser-first');
-          } else {
-            containerClass.push('sk-msg-appmaker-first');
-          }
+            if (isAppUser) {
+                containerClass.push('sk-msg-appuser-first');
+            } else {
+                containerClass.push('sk-msg-appmaker-first');
+            }
         }
         if (this.props.lastInGroup && !this.props.firstInGroup) {
-          if (isAppUser) {
-            containerClass.push('sk-msg-appuser-last');
-          } else {
-            containerClass.push('sk-msg-appmaker-last');
-          }
+            if (isAppUser) {
+                containerClass.push('sk-msg-appuser-last');
+            } else {
+                containerClass.push('sk-msg-appmaker-last');
+            }
         }
         if (!this.props.firstInGroup && !this.props.lastInGroup) {
-          if (isAppUser) {
-            containerClass.push('sk-msg-appuser-middle');
-          } else {
-            containerClass.push('sk-msg-appmaker-middle');
-          }
+            if (isAppUser) {
+                containerClass.push('sk-msg-appuser-middle');
+            } else {
+                containerClass.push('sk-msg-appmaker-middle');
+            }
         }
 
         const fromName = <div className='sk-from'>
-                           { isAppUser ? '' : this.props.name }
-                       </div>;
+                             { isAppUser ? '' : this.props.name }
+                         </div>;
 
         return <div className={ 'sk-row ' + (isAppUser ? 'sk-right-row' : 'sk-left-row') }>
-                   {!isAppUser && this.props.firstInGroup ? fromName : ''}
+                   { !isAppUser && this.props.firstInGroup ? fromName : '' }
                    { this.props.lastInGroup ? avatar : avatarPlaceHolder }
                    <div className='sk-msg-wrapper'>
                        <div className={ containerClass.join(' ') }
-                            style={ style }>
+                            style={ style }
+                            ref='messageContent'>
                            { message }
                            { actions }
                        </div>
