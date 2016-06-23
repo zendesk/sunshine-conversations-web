9a70c2231fa8a92378d8ae80f95588b238c20bed
Chloe Pou-Prom <chloe@smooch.io>
2016-06-23 10:37:28 -0400

    Refactor to use functions of child element

--- test/utils/parent-component.jsx
+++ test/utils/parent-component.jsx
@@ -1,10 +1,26 @@
 import { Provider } from 'react-redux';
 import React, { PropTypes, Component } from 'react';
 
+// Element with context:
+// component = TestUtils.renderIntoDocument(<ParentComponentWithContext context={ context } 
+//                                                                      store={ mockedStore }
+//                                                                      <ActionComponent {...props} />
+//                                          </ParentComponentWithContext>);
+
+// If you need to test functions:
+// component = TestUtils.renderIntoDocument(<ParentComponentWithContext context={ context } 
+//                                                                      store={ mockedStore }
+//                                                                      accessFunction='true'
+//                                                                      <ActionComponent {...props} />
+//                                          </ParentComponentWithContext>);
+// component.refs.childElement.someFunction();
+
 export class ParentComponentWithContext extends Component {
 
     static propTypes = {
-        context: PropTypes.object
+        context: PropTypes.object.required,
+        store: PropTypes.object.required,
+        accessFunction: PropTypes.string
     };
 
     static childContextTypes = {
@@ -18,10 +34,17 @@ export class ParentComponentWithContext extends Component {
     }
 
     render() {
+        // If we need to use functions on the child element, then add a 'ref' propType
+        const childElement = this.props.accessFunction ? React.Children.map(
+            this.props.children, function(child) {
+                return React.cloneElement(child, {
+                    ref: 'childElement'
+                });
+            }) : this.props.children;
         return (
             <Provider store={ this.props.store }>
                 <div>
-                    { this.props.children }
+                    { childElement }
                 </div>
             </Provider>
         );
