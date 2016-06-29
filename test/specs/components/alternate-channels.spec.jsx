08b7c111f10d6abdc701e81d77ac229e8eb00e43
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2016-06-29 13:21:43 -0400

    add tests for alternate channels

---
+++ test/specs/components/alternate-channels.spec.jsx
@@ -0,0 +1,45 @@
+import sinon from 'sinon';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+
+import { AlternateChannels } from '../../../src/js/components/alternate-channels';
+import { getAppChannelDetails } from '../../../src/js/utils/app';
+const appService = require('../../../src/js/services/app-service');
+
+const sandbox = sinon.sandbox.create();
+
+
+describe('AlternateChannels', () => {
+
+    beforeEach(() => {
+        sandbox.stub(appService, 'showChannelPage');
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('should render no items if no channels', () => {
+        const component = TestUtils.renderIntoDocument(<AlternateChannels items={ [] } />);
+        TestUtils.scryRenderedDOMComponentsWithClass(component, 'channel-icon').length.should.be.eq(0);
+    });
+
+    it('should render all the channels and call showChannelPage with channel type when clicked', () => {
+        const channels = [{
+            type: 'messenger'
+        }, {
+            type: 'telegram'
+        }];
+
+        const items = getAppChannelDetails(channels);
+        const component = TestUtils.renderIntoDocument(<AlternateChannels items={ items } />);
+        const itemComponents = TestUtils.scryRenderedDOMComponentsWithClass(component, 'channel-icon');
+        itemComponents.length.should.be.eq(2);
+
+        itemComponents.forEach((c, index) => {
+            TestUtils.Simulate.click(c);
+            appService.showChannelPage.should.have.been.calledWith(channels[index].type);
+        });
+    });
+
+});
