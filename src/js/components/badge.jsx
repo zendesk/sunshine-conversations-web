2cd880d63f506b77d0648f475907348f86f324ca
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2016-07-12 15:12:04 -0400

    tweak svg shadow

--- src/js/components/badge.jsx
+++ src/js/components/badge.jsx
@@ -4,18 +4,34 @@ import { openWidget } from '../services/app-service';
 
 class DefaultIcon extends Component {
     render() {
-        const {brandColorDark} = this.props;
-        const style = {
-            fill: brandColorDark ? '#fff' : '#000'
-        };
+        const {isBrandColorDark, brandColor} = this.props;
 
         return <svg version='1.0'
                     x='0px'
                     y='0px'
                     viewBox='0 0 100 100'
                     className='default-icon'
-                    style={ { enableBackground: 'new 0 0 100 100' } }>
-                   <path style={ style }
+                    style={ { enableBackground: 'new 0 0 100 100', overflow: 'visible', shapeRendering: 'geometricPrecision' } }>
+                   <filter id='dropShadow'>
+                       <feGaussianBlur stdDeviation='0,4'
+                                       in='SourceAlpha' />
+                       <feOffset dx='0'
+                                 dy='4'
+                                 result='offsetblur' />
+                       <feFlood flood-color={ `#${brandColor}` } />
+                       <feComponentTransfer>
+                           <feFuncA type='linear'
+                                    slope='0.4' />
+                       </feComponentTransfer>
+                       <feComposite operator='in'
+                                    in2='offsetblur' />
+                       <feMerge>
+                           <feMergeNode/>
+                           <feMergeNode in='SourceGraphic' />
+                       </feMerge>
+                   </filter>
+                   <path fill={ isBrandColorDark ? '#fff' : '#55555d' }
+                         filter='url(#dropShadow)'
                          d='M50,0C22.4,0,0,22.4,0,50s22.4,50,50,50h30.8l0-10.6C92.5,80.2,100,66,100,50C100,22.4,77.6,0,50,0z M32,54.5 c-2.5,0-4.5-2-4.5-4.5c0-2.5,2-4.5,4.5-4.5s4.5,2,4.5,4.5C36.5,52.5,34.5,54.5,32,54.5z M50,54.5c-2.5,0-4.5-2-4.5-4.5 c0-2.5,2-4.5,4.5-4.5c2.5,0,4.5,2,4.5,4.5C54.5,52.5,52.5,54.5,50,54.5z M68,54.5c-2.5,0-4.5-2-4.5-4.5c0-2.5,2-4.5,4.5-4.5 s4.5,2,4.5,4.5C72.5,52.5,70.5,54.5,68,54.5z' />
                </svg>;
 
@@ -33,7 +49,7 @@ export class Badge extends Component {
     };
 
     render() {
-        const {settings: {brandColor, brandColorDark, badgeIconUrl}} = this.context;
+        const {settings: {brandColor, isBrandColorDark, badgeIconUrl}} = this.context;
 
         const style = {
             backgroundColor: `#${brandColor}`
@@ -42,7 +58,8 @@ export class Badge extends Component {
         return <div className='sk-badge'
                     style={ style }
                     onClick={ this.onClick }>
-                   <DefaultIcon brandColorDark={ brandColorDark } />
+                   <DefaultIcon isBrandColorDark={ isBrandColorDark }
+                                brandColor={ brandColor } />
                </div>;
     }
 }
