7e355fdafadf8bdbe6bbd4170dc9159a63de27e3
jugarrit <julian@smooch.io>
2016-07-22 15:54:02 -0400

    Add flag selector to twilio channel (#315)
    * Add flag selector to twilio channel
    
    * Tweak css for arrow

--- src/js/components/channels/twilio-channel-content.jsx
+++ src/js/components/channels/twilio-channel-content.jsx
@@ -1,25 +1,34 @@
-import React, { Component, PropTypes } from 'react';
-import isMobile from 'ismobilejs';
+import React, { Component } from 'react';
+
+import { ReactTelephoneInput } from '../../lib/react-telephone-input';
 
 export class TwilioChannelContent extends Component {
-    static contextTypes = {
-        settings: PropTypes.object.isRequired
+    state = {
+        numberValid: false
+    }
+
+
+    onNumberValid = () => {
+        this.setState({
+            numberValid: true
+        });
+    }
+
+    onNumberInvalid = () => {
+        this.setState({
+            numberValid: false
+        });
     }
 
     render() {
-        const {settings} = this.context;
-        const {phoneNumber} = this.props;
-        const styleOverride = settings.linkColor ? {
-            color: `#${settings.linkColor}`
-        } : null;
-
-        if (isMobile.any) {
-            return <a href={ `sms:${phoneNumber}` }
-                      style={ styleOverride }>
-                       { phoneNumber }
-                   </a>;
-
-        }
-        return <span className='channel-content-value'>{ phoneNumber }</span>;
+        return <div>
+                   <ReactTelephoneInput ref='telInput'
+                                        defaultCountry='ca'
+                                        onChange={ this.handleInputChange }
+                                        onValid={ this.onNumberValid }
+                                        onInvalid={ this.onNumberInvalid }
+                                        preferredCountries={ ['ca', 'us'] }
+                                        onBlur={ this.handleInputBlur } />
+               </div>;
     }
 }
