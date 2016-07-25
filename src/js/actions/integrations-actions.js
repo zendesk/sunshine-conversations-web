ee9c7bce5d0fafb3414b8d39f0dbf4914fc067fd
chloepouprom <chloe@smooch.io>
2016-07-25 15:42:29 -0400

    Add UI for SMS linking (#325)
    * Add actions for updating state of twilio attributes
    
    * Add UI for twilio linking
    
    * Get rid of string ref
    
    * Check if device is phone
    
    * Track tel number in onChange
    
    * Use same style for email settings button
    
    * Fix onRetry
    
    * Refactor linkedComponent

--- src/js/actions/integrations-actions.js
+++ src/js/actions/integrations-actions.js
@@ -1,6 +1,8 @@
 export const SET_WECHAT_QR_CODE = 'SET_WECHAT_QR_CODE';
 export const SET_WECHAT_ERROR = 'SET_WECHAT_ERROR';
 export const UNSET_WECHAT_ERROR = 'UNSET_WECHAT_ERROR';
+export const SET_TWILIO_INTEGRATION_STATE = 'SET_TWILIO_INTEGRATION_STATE';
+export const RESET_TWILIO_INTEGRATION_STATE = 'RESET_TWILIO_INTEGRATION_STATE';
 
 export function setWeChatQRCode(code) {
     return {
@@ -18,5 +20,18 @@ export function setWeChatError() {
 export function unsetWeChatError() {
     return {
         type: UNSET_WECHAT_ERROR
+    };
+}
+
+export function setTwilioIntegrationState(attrs) {
+    return {
+        type: SET_TWILIO_INTEGRATION_STATE,
+        attrs
+    };
+}
+
+export function resetTwilioIntegrationState() {
+    return {
+        type: RESET_TWILIO_INTEGRATION_STATE
     };
 }
--- src/js/components/channels/twilio-channel-content.jsx
+++ src/js/components/channels/twilio-channel-content.jsx
@@ -1,34 +1,132 @@
 import React, { Component } from 'react';
+import { updateTwilioAttributes, resetTwilioAttributes } from '../../services/integrations-service';
 
 import { ReactTelephoneInput } from '../../lib/react-telephone-input';
 
-export class TwilioChannelContent extends Component {
-    state = {
-        numberValid: false
+import { connect } from 'react-redux';
+import isMobile from 'ismobilejs';
+
+export class TwilioChannelContentComponent extends Component {
+
+    linkTwilioNumber = () => {
+        updateTwilioAttributes({
+            number: this.props.number,
+            linkState: 'pending'
+        });
     }
 
+    onRetry = () => {
+        updateTwilioAttributes({
+            linkState: 'unlinked'
+        });
+    }
+
+    handleInputChange = (telNumber) => {
+        updateTwilioAttributes({
+            number: telNumber
+        });
+    }
+
+    onChangeNumber = () => {
+        updateTwilioAttributes({
+            linkState: 'unlinked',
+            number: '',
+            numberValid: false
+        });
+    }
+
+    onStartTexting = () => {
+        updateTwilioAttributes({
+            linkState: 'linked'
+        });
+    }
+
+    onSendText = () => {
+        updateTwilioAttributes({
+            linkState: 'linked'
+        });
+    }
 
     onNumberValid = () => {
-        this.setState({
+        updateTwilioAttributes({
             numberValid: true
         });
     }
 
     onNumberInvalid = () => {
-        this.setState({
+        updateTwilioAttributes({
             numberValid: false
         });
     }
 
+    componentWillUnmount() {
+        resetTwilioAttributes();
+    }
+
     render() {
-        return <div>
-                   <ReactTelephoneInput ref='telInput'
-                                        defaultCountry='ca'
-                                        onChange={ this.handleInputChange }
-                                        onValid={ this.onNumberValid }
-                                        onInvalid={ this.onNumberInvalid }
-                                        preferredCountries={ ['ca', 'us'] }
-                                        onBlur={ this.handleInputBlur } />
-               </div>;
+        const {number, numberValid, phoneNumber, linkState, settings: {linkColor}} = this.props;
+        let iconStyle = {};
+        if (linkColor) {
+            iconStyle = {
+                color: `#${linkColor}`
+            };
+        }
+
+        const linkButton = numberValid ? <button className='btn btn-sk-primary'
+                                                 onClick={ this.linkTwilioNumber }>
+                                             Continue
+                                         </button> : '';
+        const unlinkedComponent = <div>
+                                      <ReactTelephoneInput ref={ (c) => this._telInput = c }
+                                                           defaultCountry='ca'
+                                                           onChange={ this.handleInputChange }
+                                                           onValid={ this.onNumberValid }
+                                                           onInvalid={ this.onNumberInvalid }
+                                                           preferredCountries={ ['ca', 'us'] }
+                                                           onBlur={ this.handleInputBlur } />
+                                      { linkButton }
+                                  </div>;
+        const pendingComponent = <div className='twilio-linking pending-state'>
+                                     <i className='fa fa-phone'
+                                        style={ iconStyle }></i>
+                                     <span className='phone-number'>{ number } - Pending</span>
+                                     <a onClick={ this.onRetry }>Retry</a>
+                                 </div>;
+
+        const sendTextUrl = `sms://${phoneNumber}`;
+        const linkedComponentButton = isMobile.phone ? <button className='btn btn-sk-primary twilio-linking'
+                                                               onClick={ this.onStartTexting }>
+                                                           Start Texting
+                                                       </button> :
+            <button className='btn btn-sk-primary twilio-linking'
+                    onClick={ this.onSendText }>
+                Send me a text
+            </button>;
+
+        const linkedComponent = <div>
+                                    <div className='twilio-linking linked-state'>
+                                        <i className='fa fa-phone'
+                                           style={ iconStyle }></i>
+                                        <span className='phone-number'>{ number }</span>
+                                        <a onClick={ this.onChangeNumber }>Change my number</a>
+                                    </div>
+                                    <a href={ sendTextUrl }>
+                                        { linkedComponentButton }
+                                    </a>
+                                </div>;
+        if (linkState === 'pending') {
+            return pendingComponent;
+        } else if (linkState === 'linked') {
+            return linkedComponent;
+        } else {
+            return unlinkedComponent;
+        }
     }
 }
+
+export const TwilioChannelContent = connect((state) => {
+    return {
+        ...state.integrations.twilio,
+        settings: state.app.settings.web
+    };
+})(TwilioChannelContentComponent);
