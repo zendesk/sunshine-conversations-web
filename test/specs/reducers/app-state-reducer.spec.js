e002810428fd539453b096ee06f964cf55b0929b
Chloe Pou-Prom <chloe@smooch.io>
2016-08-03 10:56:56 -0400

    Fix tests

--- test/specs/reducers/app-state-reducer.spec.js
+++ test/specs/reducers/app-state-reducer.spec.js
@@ -1,5 +1,5 @@
 import { AppStateReducer } from '../../../src/js/reducers/app-state-reducer';
-import { TOGGLE_WIDGET, OPEN_WIDGET, CLOSE_WIDGET } from '../../../src/js/actions/app-state-actions';
+import { TOGGLE_WIDGET, OPEN_WIDGET, CLOSE_WIDGET, DISABLE_ANIMATION } from '../../../src/js/actions/app-state-actions';
 
 const INITIAL_STATE = AppStateReducer(undefined, {});
 
@@ -7,12 +7,28 @@ describe('App State reducer', () => {
     [TOGGLE_WIDGET, OPEN_WIDGET, CLOSE_WIDGET].forEach((action) => {
         describe(`${action} action`, () => {
             it('should set the showAnimation flag to true', () => {
-                const beforeState = INITIAL_STATE;
+                const beforeState = {
+                    ...INITIAL_STATE,
+                    showAnimation: false
+                };
                 const afterState = AppStateReducer(beforeState, {
                     type: action
                 });
                 afterState.showAnimation.should.eq(true);
             });
+        });
+    });
+
+    describe('DISABLE_ANIMATION action', () => {
+        it('should set the showAnimation flag to false', () => {
+            const beforeState = {
+                ...INITIAL_STATE,
+                showAnimation: true
+            };
+            const afterState = AppStateReducer(beforeState, {
+                type: DISABLE_ANIMATION
+            });
+            afterState.showAnimation.should.eq(false);
         });
     });
 });
