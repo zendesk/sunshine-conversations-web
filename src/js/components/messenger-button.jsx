59e3a7e924d75baba1c1796c3bcb2a2573985b36
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2016-11-10 13:25:01 -0500

    remove leftovers of attempt to fix

--- src/js/components/messenger-button.jsx
+++ src/js/components/messenger-button.jsx
@@ -1,14 +1,10 @@
 import React, { Component, PropTypes } from 'react';
-import { findDOMNode } from 'react-dom';
 import { connect } from 'react-redux';
 
 import { openWidget } from '../services/app-service';
 import { SK_DARK_CONTRAST } from '../constants/styles';
 
 export class DefaultButtonIcon extends Component {
-    componentDidMount() {
-        this._pathNode.filter = this._pathNode.filter;
-    }
 
     render() {
         const {isBrandColorDark} = this.props;
@@ -38,8 +34,7 @@ export class DefaultButtonIcon extends Component {
                            <feMergeNode in='SourceGraphic' />
                        </feMerge>
                    </filter>
-                   <path ref={ (c) => this._pathNode = findDOMNode(c) }
-                         fill={ isBrandColorDark ? '#fff' : SK_DARK_CONTRAST }
+                   <path fill={ isBrandColorDark ? '#fff' : SK_DARK_CONTRAST }
                          filter={ `url(${protocol}//${host}${pathname}${search}#dropShadow)` }
                          d='M50,0C22.4,0,0,22.4,0,50s22.4,50,50,50h30.8l0-10.6C92.5,80.2,100,66,100,50C100,22.4,77.6,0,50,0z M32,54.5 c-2.5,0-4.5-2-4.5-4.5c0-2.5,2-4.5,4.5-4.5s4.5,2,4.5,4.5C36.5,52.5,34.5,54.5,32,54.5z M50,54.5c-2.5,0-4.5-2-4.5-4.5 c0-2.5,2-4.5,4.5-4.5c2.5,0,4.5,2,4.5,4.5C54.5,52.5,52.5,54.5,50,54.5z M68,54.5c-2.5,0-4.5-2-4.5-4.5c0-2.5,2-4.5,4.5-4.5 s4.5,2,4.5,4.5C72.5,52.5,70.5,54.5,68,54.5z' />
                </svg>;
