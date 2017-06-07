416489981d17c1812073974e1417b80857b23b70
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2017-06-07 17:10:36 -0400

    Revert webpack code splitting

--- src/frame/js/components/settings.jsx
+++ src/frame/js/components/settings.jsx
@@ -1,60 +1,22 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-import { SettingsLoading } from './settings-loading';
+import { EmailSettings } from './email-settings';
+import { NotificationsSettings } from './notifications-settings';
 import { hasChannels } from '../utils/app';
 
-let NotificationsSettings;
-let EmailSettings;
-
 export class SettingsComponent extends Component {
+
     static propTypes = {
         settings: PropTypes.object.isRequired
     };
 
-    state = {
-        hasComponent: !!this.getComponent()
-    };
-
-    componentWillMount() {
-        if(!this.state.hasComponent) {
-            this.loadComponent();
-        }
-    }
-
-    componentWillUpdate(nextProps) {
-        if(!this.getComponent(nextProps)) {
-            this.loadComponent();
-        }
-    }
-
-    loadComponent(props = this.props) {
-        const {settings} = props;
-        if(hasChannels(settings)) {
-            import('./notifications-settings')
-                .then((Component) => {
-                    NotificationsSettings = Component.NotificationsSettings;
-                    this.setState({hasComponent: true});
-                });
-        } else {
-            import('./email-settings')
-                .then((Component) => {
-                    EmailSettings = Component.EmailSettings;
-                    this.setState({hasComponent: true});
-                });
-        }
-    }
-
-    getComponent(props = this.props) {
-        const {settings} = props;
-        return hasChannels(settings) ? NotificationsSettings : EmailSettings;
-    }
-
     render() {
-        const SettingsComponent = this.getComponent();
+        const {settings} = this.props;
+        const settingsComponent = hasChannels(settings) ? <NotificationsSettings /> : <EmailSettings />;
 
         return <div className='sk-settings'>
-                   { SettingsComponent ? <SettingsComponent /> : <SettingsLoading /> }
+                   { settingsComponent }
                </div>;
     }
 }
