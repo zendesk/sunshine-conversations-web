ad14515ec2279c65d9de29bd16bb3f512b1dd110
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2017-06-09 11:54:32 -0400

    Update dependencies for React 15.5

--- src/frame/js/components/text-message.jsx
+++ src/frame/js/components/text-message.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { createMarkup, autolink, escapeHtml } from '../utils/html';
 
 export class TextMessage extends Component {
     static propTypes = {
-        text: React.PropTypes.string.isRequired,
-        className: React.PropTypes.string,
-        role: React.PropTypes.string.isRequired
+        text: PropTypes.string.isRequired,
+        className: PropTypes.string,
+        role: PropTypes.string.isRequired
     };
 
     render() {
---
+++ src/frame/js/lib/react-imageloader.jsx
@@ -0,0 +1,138 @@
+/* eslint-disable */
+// Vendored from https://github.com/hzdg/react-imageloader
+// because of the lack of activity in the repository and needed to be updated
+import React from 'react';
+import PropTypes from 'prop-types';
+const {span} = React.DOM;
+
+const Status = {
+  PENDING: 'pending',
+  LOADING: 'loading',
+  LOADED: 'loaded',
+  FAILED: 'failed',
+};
+
+
+export default class ImageLoader extends React.Component {
+  static propTypes = {
+    wrapper: PropTypes.func,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    preloader: PropTypes.func,
+    src: PropTypes.string,
+    onLoad: PropTypes.func,
+    onError: PropTypes.func,
+    imgProps: PropTypes.object,
+  };
+
+  static defaultProps = {
+    wrapper: span,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {status: props.src ? Status.LOADING : Status.PENDING};
+  }
+
+  componentDidMount() {
+    if (this.state.status === Status.LOADING) {
+      this.createLoader();
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.src !== nextProps.src) {
+      this.setState({
+        status: nextProps.src ? Status.LOADING : Status.PENDING,
+      });
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.status === Status.LOADING && !this.img) {
+      this.createLoader();
+    }
+  }
+
+  componentWillUnmount() {
+    this.destroyLoader();
+  }
+
+  getClassName() {
+    let className = `imageloader ${this.state.status}`;
+    if (this.props.className) className = `${className} ${this.props.className}`;
+    return className;
+  }
+
+  createLoader() {
+    this.destroyLoader();  // We can only have one loader at a time.
+
+    this.img = new Image();
+    this.img.onload = this.handleLoad;
+    this.img.onerror = this.handleError;
+    this.img.src = this.props.src;
+  }
+
+  destroyLoader() {
+    if (this.img) {
+      this.img.onload = null;
+      this.img.onerror = null;
+      this.img = null;
+    }
+  }
+
+  handleLoad = (event) => {
+    this.destroyLoader();
+    this.setState({status: Status.LOADED});
+
+    if (this.props.onLoad) this.props.onLoad(event);
+  };
+
+  handleError = (error) => {
+    this.destroyLoader();
+    this.setState({status: Status.FAILED});
+
+    if (this.props.onError) this.props.onError(error);
+  };
+
+  renderImg() {
+    const {src, imgProps} = this.props;
+    const props = {src};
+
+    for (const k in imgProps) {
+      if (imgProps.hasOwnProperty(k)) {
+        props[k] = imgProps[k];
+      }
+    }
+
+    return <img {...props} />;
+  }
+
+  render() {
+    const wrapperProps = {
+      className: this.getClassName(),
+    };
+
+    if (this.props.style) {
+      wrapperProps.style = this.props.style;
+    }
+
+    const wrapperArgs = [wrapperProps];
+
+    switch (this.state.status) {
+      case Status.LOADED:
+        wrapperArgs.push(this.renderImg());
+        break;
+
+      case Status.FAILED:
+        if (this.props.children) wrapperArgs.push(this.props.children);
+        break;
+
+      default:
+        if (this.props.preloader) wrapperArgs.push(this.props.preloader());
+        break;
+    }
+
+    return this.props.wrapper(...wrapperArgs);
+  }
+}
