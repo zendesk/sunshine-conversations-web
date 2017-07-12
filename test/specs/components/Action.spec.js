e93e38f40198a6216f6fda5469689b7dae51d20c
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2017-07-12 15:15:53 -0400

    Use default exports and capitalize filenames

---
+++ test/specs/components/Action.spec.js
@@ -0,0 +1,412 @@
+import sinon from 'sinon';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import deepAssign from 'deep-assign';
+
+import { createMockedStore } from '../../utils/redux';
+import { mockComponent, wrapComponentWithStore } from '../../utils/react';
+
+import StripeCheckout from '../../../src/frame/js/lib/react-stripe-checkout';
+import Action, { __Rewire__ as ActionRewire } from '../../../src/frame/js/components/Action';
+import Loading from '../../../src/frame/js/components/Loading';
+
+const sandbox = sinon.sandbox.create();
+
+function getNormalProps(props = {}) {
+    const defaultProps = {
+        _id: 'action id',
+        type: 'link',
+        text: 'action text',
+        uri: 'http://some-uri/'
+    };
+
+    return Object.assign(defaultProps, props);
+}
+
+function getBuyProps(props = {}) {
+    const defaultProps = {
+        _id: 'action id',
+        type: 'buy',
+        text: 'action text',
+        uri: 'http://some-uri/',
+        currency: 'usd',
+        state: 'offered'
+    };
+
+    return Object.assign(defaultProps, props);
+}
+
+function getPostbackProps(props = {}) {
+    const defaultProps = {
+        _id: 'postback id',
+        type: 'postback',
+        text: 'postback text',
+        payload: 'postback payload'
+    };
+
+    return Object.assign(defaultProps, props);
+}
+
+function getStoreState(state = {}) {
+    const defaultState = {
+        app: {
+            integrations: []
+        },
+        user: {
+            email: 'test'
+        },
+        ui: {
+            text: {
+                actionPaymentCompleted: 'payment completed text'
+            }
+        }
+    };
+
+    return deepAssign(defaultState, state);
+}
+
+describe('Action Component', () => {
+    let component;
+    let componentNode;
+    let mockedStore;
+    let immediateUpdateStub;
+    let postPostbackStub;
+    let getIntegrationStub;
+    let createTransactionStub;
+
+    beforeEach(() => {
+        mockComponent(sandbox, StripeCheckout, 'div', {
+            className: 'mockedStripe'
+        });
+        mockComponent(sandbox, Loading, 'div', {
+            className: 'mockedLoading'
+        });
+
+
+        immediateUpdateStub = sandbox.stub().returnsAsyncThunk();
+        ActionRewire('immediateUpdate', immediateUpdateStub);
+
+        postPostbackStub = sandbox.stub().returnsAsyncThunk();
+        ActionRewire('postPostback', postPostbackStub);
+
+        createTransactionStub = sandbox.stub().returnsAsyncThunk();
+        ActionRewire('createTransaction', createTransactionStub);
+
+        getIntegrationStub = sandbox.stub().returns(
+            {
+                type: 'stripeConnect',
+                publicKey: 'key'
+            }
+        );
+        ActionRewire('getIntegration', getIntegrationStub);
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    describe('normal action link', () => {
+        const props = getNormalProps();
+
+        beforeEach(() => {
+            mockedStore = createMockedStore(sandbox, getStoreState());
+            component = wrapComponentWithStore(Action, props, mockedStore);
+            componentNode = ReactDOM.findDOMNode(component);
+        });
+
+        it('should render a link with target blank', () => {
+            const link = TestUtils.findRenderedDOMComponentWithTag(component, 'a');
+            link.textContent.should.eq('action text');
+            link.href.should.eq('http://some-uri/');
+            link.target.should.eq('_blank');
+        });
+    });
+
+    describe('javascript action link', () => {
+        const props = getNormalProps({
+            uri: 'javascript:someAction()'
+        });
+
+        beforeEach(() => {
+            mockedStore = createMockedStore(sandbox, getStoreState());
+            component = wrapComponentWithStore(Action, props, mockedStore);
+            componentNode = ReactDOM.findDOMNode(component);
+        });
+
+        it('should render a link with target self', () => {
+            const link = TestUtils.findRenderedDOMComponentWithTag(component, 'a');
+            link.textContent.should.eq('action text');
+            link.href.should.eq('javascript:someAction()');
+            link.target.should.eq('_self');
+        });
+    });
+
+    describe('Stripe', () => {
+        const storeState = getStoreState({
+            app: {
+                integrations: [
+                    {
+                        type: 'stripeConnect'
+                    }
+                ],
+                stripe: {
+                    appName: 'app-name',
+                    iconUrl: 'iconUrl'
+                }
+            }
+        });
+
+        beforeEach(() => {
+            mockedStore = createMockedStore(sandbox, storeState);
+        });
+
+        afterEach(() => {
+            getIntegrationStub.should.have.been.calledWithMatch(storeState.app.integrations, 'stripeConnect');
+        });
+
+        describe('buy action with stripe keys and offered state', () => {
+            const props = getBuyProps({
+                uri: 'http://some-fallback-uri/'
+            });
+
+            beforeEach(() => {
+                component = wrapComponentWithStore(Action, props, mockedStore);
+            });
+
+            it('should render a Stripe button', () => {
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'mockedStripe').length.should.be.eq(1);
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'mockedLoading').length.should.be.eq(0);
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'btn-sk-action-paid').length.should.be.eq(0);
+                componentNode.textContent.should.eq('action text');
+            });
+        });
+
+        describe('buy action with stripe keys and processing state', () => {
+            const props = getBuyProps({
+                state: 'processing'
+            });
+
+            beforeEach(() => {
+                component = wrapComponentWithStore(Action, props, mockedStore);
+                componentNode = ReactDOM.findDOMNode(component);
+            });
+
+            it('should render a loading element', () => {
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'mockedStripe').length.should.be.eq(0);
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'mockedLoading').length.should.be.eq(1);
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'btn-sk-action-paid').length.should.be.eq(0);
+                componentNode.textContent.should.eq('');
+            });
+        });
+
+        describe('buy action with stripe keys and paid state', () => {
+            const props = getBuyProps({
+                state: 'paid'
+            });
+
+            beforeEach(() => {
+                component = wrapComponentWithStore(Action, props, mockedStore);
+                componentNode = ReactDOM.findDOMNode(component);
+            });
+
+            it('should show a payment completed button', () => {
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'btn-sk-action-paid').length.should.be.eq(1);
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'mockedStripe').length.should.be.eq(0);
+                TestUtils.scryRenderedDOMComponentsWithClass(component, 'mockedLoading').length.should.be.eq(0);
+                componentNode.textContent.should.eq('payment completed text');
+            });
+        });
+
+        describe('onStripeToken', () => {
+
+            describe('user has no email', () => {
+                const props = getBuyProps();
+
+                beforeEach(() => {
+                    mockedStore = createMockedStore(sandbox, getStoreState({
+                        app: {
+                            integrations: [
+                                {
+                                    type: 'stripeConnect'
+                                }
+                            ],
+                            stripe: {
+                                appName: 'app-name',
+                                iconUrl: 'iconUrl'
+                            }
+                        },
+                        user: {
+                            email: ''
+                        }
+                    }));
+
+                    component = wrapComponentWithStore(Action, props, mockedStore);
+                });
+
+                it('call user update', () => {
+                    return component.getWrappedInstance().onStripeToken({
+                        email: 'email'
+                    }).then(() => {
+                        immediateUpdateStub.should.have.been.calledWith({
+                            email: 'email'
+                        });
+                    });
+                });
+            });
+
+            describe('user has email', () => {
+                const props = getBuyProps();
+                beforeEach(() => {
+                    component = wrapComponentWithStore(Action, props, mockedStore);
+                });
+
+                it('call user update', () => {
+                    return component.getWrappedInstance().onStripeToken({
+                        email: 'email'
+                    }).then(() => {
+                        immediateUpdateStub.should.not.have.been.called;
+                    });
+                });
+            });
+
+            describe('create transaction success', () => {
+                const props = getBuyProps({
+                    state: 'processing'
+                });
+
+                beforeEach(() => {
+                    component = wrapComponentWithStore(Action, props, mockedStore).getWrappedInstance();
+                });
+
+                it('should set state to paid', () => {
+                    component.state.state.should.eq('processing');
+                    return component.onStripeToken({
+                        email: 'email'
+                    }).then(() => {
+                        component.state.state.should.eq('paid');
+                    });
+                });
+            });
+
+            describe('create transaction fail', () => {
+                const props = getBuyProps({
+                    state: 'processing'
+                });
+
+                beforeEach(() => {
+                    component = wrapComponentWithStore(Action, props, mockedStore).getWrappedInstance();
+                    createTransactionStub.returnsAsyncThunk({
+                        rejects: true
+                    });
+                });
+
+                it('should set state to paid', () => {
+                    component.state.state.should.eq('processing');
+                    return component.onStripeToken({
+                        email: 'email'
+                    }).then(() => {
+                        component.state.state.should.eq('offered');
+                    });
+                });
+            });
+        });
+
+        describe('onStripeClick', () => {
+            const props = getBuyProps();
+
+            beforeEach(() => {
+                component = wrapComponentWithStore(Action, props, mockedStore).getWrappedInstance();
+            });
+
+            it('set state to processing', () => {
+                const event = {
+                    preventDefault: sandbox.stub()
+                };
+
+                component.state.state.should.eq('offered');
+                component.onStripeClick(event);
+                component.state.state.should.eq('processing');
+
+                event.preventDefault.should.have.been.calledOnce;
+            });
+        });
+
+        describe('onStripeClose', () => {
+            const props = getBuyProps({
+                state: 'processing'
+            });
+
+            beforeEach(() => {
+                component = wrapComponentWithStore(Action, props, mockedStore).getWrappedInstance();
+            });
+
+            it('should set state to offered if no token', () => {
+                component.state.state.should.eq('processing');
+                component.state.hasToken = false;
+                component.onStripeClose();
+                component.state.state.should.eq('offered');
+            });
+
+            it('should do nothing if token', () => {
+                component.state.state.should.eq('processing');
+                component.state.hasToken = true;
+                component.onStripeClose();
+                component.state.state.should.eq('processing');
+            });
+        });
+    });
+
+    describe('buy action without stripe keys', () => {
+        const props = getBuyProps();
+        const storeState = getStoreState({
+            app: {
+                stripe: {
+                    appName: 'app-name',
+                    iconUrl: 'iconUrl'
+                }
+            },
+            ui: {
+                text: {
+                    actionPaymentCompleted: 'payment completed text'
+                }
+            }
+        });
+
+        beforeEach(() => {
+            getIntegrationStub.returns(undefined);
+            mockedStore = createMockedStore(sandbox, storeState);
+            component = wrapComponentWithStore(Action, props, mockedStore).getWrappedInstance();
+        });
+
+        it('should render a link', () => {
+            const link = TestUtils.findRenderedDOMComponentWithTag(component, 'a');
+            link.textContent.should.eq('action text');
+            link.href.should.eq('http://some-uri/');
+            link.target.should.eq('_blank');
+        });
+    });
+
+    describe('postback action', () => {
+        const props = getPostbackProps();
+
+        beforeEach(() => {
+            component = wrapComponentWithStore(Action, props, mockedStore).getWrappedInstance();
+            componentNode = ReactDOM.findDOMNode(component);
+        });
+
+        it('should render a postback', () => {
+            componentNode.textContent.should.eq('postback text');
+        });
+
+        it('should call the postPostback action on button click', (done) => {
+            const button = TestUtils.findRenderedDOMComponentWithTag(component, 'a');
+            TestUtils.Simulate.click(button);
+
+            // use setTimeout to let the promise chain resolve in onPostbackClick
+            setTimeout(() => {
+                postPostbackStub.should.have.been.calledOnce;
+                done();
+            });
+        });
+    });
+});
---
+++ test/specs/components/Introduction.spec.js
@@ -0,0 +1,139 @@
+import sinon from 'sinon';
+import TestUtils from 'react-addons-test-utils';
+import deepAssign from 'deep-assign';
+
+import Introduction from '../../../src/frame/js/components/Introduction';
+import AlternateChannels from '../../../src/frame/js/components/AlternateChannels';
+import DefaultAppIcon from '../../../src/frame/js/components/DefaultAppIcon';
+
+import { mockComponent, wrapComponentWithStore } from '../../utils/react';
+import { createMockedStore } from '../../utils/redux';
+
+const sandbox = sinon.sandbox.create();
+
+function getStoreState(state = {}) {
+    const defaultState = {
+        app: {
+            name: 'some-name',
+            settings: {
+                web: {
+                    channels: {}
+                }
+            },
+            integrations: []
+        },
+        ui: {
+            text: {
+                introductionText: 'introductionText',
+                introAppText: 'introAppText'
+            }
+        },
+        appState: {
+            introHeight: 0
+        },
+        conversation: {
+            unreadCount: 0
+        }
+    };
+
+    return deepAssign(defaultState, state);
+}
+
+describe('Introduction Component', () => {
+    let mockedStore;
+    let props;
+
+    beforeEach(() => {
+        mockComponent(sandbox, AlternateChannels, 'div', {
+            className: 'mockedAlternateChannels'
+        });
+        mockComponent(sandbox, DefaultAppIcon, 'div', {
+            className: 'mockedDefaultAppIcon'
+        });
+
+        props = {
+            dispatch: sandbox.spy()
+        };
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('should render the app name', () => {
+        mockedStore = createMockedStore(sandbox, getStoreState());
+        const component = wrapComponentWithStore(Introduction, props, mockedStore);
+
+        const node = TestUtils.findRenderedDOMComponentWithClass(component, 'app-name');
+        node.textContent.should.eq('some-name');
+    });
+
+    it('should use app icon if provided', () => {
+        mockedStore = createMockedStore(sandbox, getStoreState({
+            app: {
+                iconUrl: 'http://some-site/some-path'
+            }
+        }));
+
+        const component = wrapComponentWithStore(Introduction, props, mockedStore);
+
+        const node = TestUtils.findRenderedDOMComponentWithClass(component, 'app-icon');
+        node.src.should.eq('http://some-site/some-path');
+        TestUtils.scryRenderedDOMComponentsWithClass(component, 'mockedDefaultAppIcon').length.should.eq(0);
+    });
+
+    it('should use default app icon if no icon url is provided', () => {
+        mockedStore = createMockedStore(sandbox, getStoreState());
+
+        const component = wrapComponentWithStore(Introduction, props, mockedStore);
+
+        TestUtils.scryRenderedDOMComponentsWithClass(component, 'mockedDefaultAppIcon').length.should.eq(1);
+        TestUtils.scryRenderedDOMComponentsWithClass(component, 'app-icon').length.should.eq(0);
+    });
+
+    it('should use intro text only if app has no integrations', () => {
+        const component = wrapComponentWithStore(Introduction, props, mockedStore);
+
+        const node = TestUtils.findRenderedDOMComponentWithClass(component, 'intro-text');
+        node.textContent.should.eq('introductionText');
+    });
+
+    it('should use intro text and intro app text if app has integrations', () => {
+        mockedStore = createMockedStore(sandbox, getStoreState({
+            app: {
+                integrations: [
+                    {
+                        type: 'messenger'
+                    }
+                ]
+            }
+        }));
+        const component = wrapComponentWithStore(Introduction, props, mockedStore);
+
+        const node = TestUtils.findRenderedDOMComponentWithClass(component, 'intro-text');
+        node.textContent.should.eq('introductionText introAppText');
+    });
+
+    it('should render the AlternateChannels component if app has integrations', () => {
+        mockedStore = createMockedStore(sandbox, getStoreState({
+            app: {
+                integrations: [
+                    {
+                        type: 'messenger'
+                    }
+                ]
+            }
+        }));
+        const component = wrapComponentWithStore(Introduction, props, mockedStore);
+
+        TestUtils.scryRenderedDOMComponentsWithClass(component, 'mockedAlternateChannels').length.should.eq(1);
+    });
+
+    it('should not render the AlternateChannels component if app has no integrations', () => {
+        mockedStore = createMockedStore(sandbox, getStoreState());
+        const component = wrapComponentWithStore(Introduction, props, mockedStore);
+
+        TestUtils.scryRenderedDOMComponentsWithClass(component, 'mockedAlternateChannels').length.should.eq(0);
+    });
+
+});
