80cd40c23b9d19c554efe8a8a81c8602fc52732b
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2017-08-18 11:55:46 -0400

    Add task to generate customized loader

---
+++ tasks/generateLoader.js
@@ -0,0 +1,21 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+let content = fs.readFileSync(path.join(__dirname, '../src/loader/index.js')).toString();
+
+if (process.env.WEBLOADER_URL) {
+    content = content.replace(
+        '\'https://\' + appId + \'.webloader.smooch.io/\'',
+        `'${process.env.WEBLOADER_URL}'`
+    );
+}
+
+if (process.env.GLOBAL_VARIABLE_NAME) {
+    content = content.replace(
+        '\'Smooch\'',
+        `'${process.env.GLOBAL_VARIABLE_NAME}'`
+    );
+}
+
+console.log(content);
